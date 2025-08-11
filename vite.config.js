7d1975844211ddd268708a19a4b4470d7f676854
woody868 <lhj303606@gmail.com>
2025-08-11 18:02:37 +0900

    dd

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vite-sample2/', // 여기에 실제 저장소 이름 입력
+  build: {
+    outDir: 'dist'
+  }
 })
